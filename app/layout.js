50a77daaf5c950da311caa1d40d85eaf00150941
tashdid-professional <tashdid.professional@gmail.com>
2025-12-05 01:29:57 +0600

    updated analytics

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import "./globals.css";
 import Navigation from "../components/Navigation";
 import Footer from "../components/Footer";
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata = {
   title: "EWU Helpdesk - Course Planning, Routine Generation & CGPA Calculator",
